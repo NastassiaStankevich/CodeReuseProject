cc1d974ddbc2a08448201ee2164e648a75435550
Paxan58 <pavelstaricov318@mail.ru>
2022-07-05 19:46:37 +0300

    get and set

--- src/by/itstep/stank/javalessons/lesson22/model/entity/Student.java
+++ src/by/itstep/stank/javalessons/lesson22/model/entity/Student.java
@@ -1,9 +1,32 @@
 package by.itstep.stank.javalessons.lesson22.model.entity;
 public class Student {
+
+    public static final int MIN_MARK =0;
+    public static final int MAX_MARK =10;
     public String name;
     public int age;
-    public double mark = 4;
+    private double mark =4;
     public boolean alive;
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+
+    }
+
+    public double getMark(){
+        return  mark;
+    }
+
+    public void setMark(double mark){
+        if (mark >= MIN_MARK && mark <= MAX_MARK ) {
+            this.mark = mark;
+        }
+    }
 
 
 
